5cfc9ab0dfe0406e9364672fb307919b13654318
rhyous <rhyous@yahoo.com>
2016-04-06 08:53:04 -0600

    Second unit test

--- Unit Tests/Rhyous.SimplePluginLoader.Tests/PluginTests.cs
+++ Unit Tests/Rhyous.SimplePluginLoader.Tests/PluginTests.cs
@@ -7,13 +7,22 @@ namespace Rhyous.SimplePluginLoader.Tests
     public class PluginTests
     {
         [TestMethod]
-        public void PdbTests()
+        public void PdbTest()
         {
             var dll = @"C:\test\library.dll";
             var pdb = @"C:\test\library.pdb";
             Plugin<ITestPlugin> plugin = new Plugin<ITestPlugin> { File = dll };
 
             Assert.AreEqual(pdb, plugin.FilePdb);
+        }
+
+        [TestMethod]
+        public void PdbTestInvalidPath()
+        {
+            var dll = @"Invalid";
+            Plugin<ITestPlugin> plugin = new Plugin<ITestPlugin> { File = dll };
+
+            Assert.AreEqual(null, plugin.FilePdb);
         }
     }
 }
