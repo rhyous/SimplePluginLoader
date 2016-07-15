194e5c229604bd4c9df48ca3dc0d60c8752e11eb
rhyous <rhyous@yahoo.com>
2016-07-15 11:11:53 -0600

    Updated IPluginLoader interface

--- Rhyous.SimplePluginLoader/Interfaces/ILoadPlugins.cs
+++ Rhyous.SimplePluginLoader/Interfaces/ILoadPlugins.cs
@@ -1,7 +1,34 @@
-﻿namespace Rhyous.SimplePluginLoader
+﻿using System.Collections.Generic;
+
+namespace Rhyous.SimplePluginLoader
 {
-    interface ILoadPlugins<T> where T : class
+    public interface ILoadPlugins<T> where T : class
     {
+        /// <summary>
+        /// Load plugins.  
+        /// </summary> 
+        /// <returns>A collection of loaded plugins.</returns>
         PluginCollection<T> LoadPlugins();
+
+        /// <summary>
+        /// Load plugins from the list of files.
+        /// </summary>
+        /// <param name="pluginFiles">A list of files.</param>
+        /// <returns>A collection of loaded plugins.</returns>
+        PluginCollection<T> LoadPlugins(IEnumerable<string> dirs);
+
+        /// <summary>
+        /// Load plugins from the list of files.
+        /// </summary>
+        /// <param name="pluginFiles">A list of files.</param>
+        /// <returns>A collection of loaded plugins.</returns>
+        PluginCollection<T> LoadPlugins(string[] pluginFiles);
+
+        /// <summary>
+        /// Load a single plugin from a specified file.
+        /// </summary>
+        /// <param name="pluginFile">The plugin file.</param>
+        /// <returns>A single Plugin.</returns>
+        Plugin<T> LoadPlugin(string pluginFile);
     }
 }
