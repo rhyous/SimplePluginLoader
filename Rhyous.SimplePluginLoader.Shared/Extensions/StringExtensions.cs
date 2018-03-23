2e8d3f493968d28f94f380c5bf58783d56d71cca
rhyous <rhyous@yahoo.com>
2018-03-23 10:24:59 -0600

    Fixed bug with Enum conversion

--- Rhyous.SimplePluginLoader.Shared/Extensions/StringExtensions.cs
+++ Rhyous.SimplePluginLoader.Shared/Extensions/StringExtensions.cs
@@ -9,10 +9,11 @@ namespace Rhyous.SimplePluginLoader.Extensions
             where T : struct, IConvertible
         {
             if (!typeof(T).IsEnum)
-                throw new ArgumentException("", "str");
-            T ret;
+                throw new ArgumentException("T must be an enum", "T");
+            if (string.IsNullOrWhiteSpace(str))
+                return defaultValue;
             var isNumeric = str.All(c => Char.IsDigit(c));
-            if ((allowNumeric || !isNumeric) && Enum.TryParse(str, ignoreCase, out ret))
+            if ((allowNumeric || !isNumeric) && Enum.TryParse(str, ignoreCase, out T ret))
                 return ret;
             return defaultValue;
         }
