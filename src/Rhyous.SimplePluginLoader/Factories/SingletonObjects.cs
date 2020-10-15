b245ff4684777450f0972e1ea17f74d4e9490835
Jared Barneck <rhyous@yahoo.com>
2020-10-14 22:28:51 -0600

    Add missing items from merge

--- src/Rhyous.SimplePluginLoader/Factories/SingletonObjects.cs
+++ src/Rhyous.SimplePluginLoader/Factories/SingletonObjects.cs
@@ -12,9 +12,10 @@
             AssemblyCache = new AssemblyCache(AppDomain, AssemblyNameReader, Logger);
             AssemblyLoader = new AssemblyLoader(AppDomain, Settings, AssemblyCache, AssemblyNameReader, Logger);
             PluginPaths = new AppPluginPaths(Settings.AppName, Settings.PluginFolder, AppDomain, Logger);
-            PluginDependencyResolverObjectCreator = new PluginDependencyResolverObjectCreator(AppDomain, Settings, AssemblyLoader, Logger);
+            Waiter = new Waiter(Logger);
+            AssemblyResolveCache = new AssemblyResolveCache();
+            PluginDependencyResolverObjectCreator = new PluginDependencyResolverObjectCreator(AppDomain, Settings, AssemblyLoader, Waiter, AssemblyResolveCache, Logger);
             PluginDependencyResolverFactory = new PluginDependencyResolverCacheFactory(PluginDependencyResolverObjectCreator, Logger);
-
         }
 
         public virtual IPluginLoaderSettings Settings { get; set; }
@@ -25,6 +26,8 @@
         public virtual IAssemblyCache AssemblyCache { get; set; }
         public virtual AssemblyLoader AssemblyLoader { get; set; }
         public virtual IPluginPaths PluginPaths { get; set; }
+        public virtual IWaiter Waiter { get; set; }
+        public virtual IAssemblyResolveCache AssemblyResolveCache { get; set; }
         public virtual IPluginDependencyResolverObjectCreator PluginDependencyResolverObjectCreator { get; set; }
         public virtual IPluginDependencyResolverCacheFactory PluginDependencyResolverFactory { get; set; }
     }
